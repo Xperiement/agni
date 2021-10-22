4c9d7c7a472a620b99b9ff24925622f45e707093
Sourav Gain <souravgain605@gmail.com>
2021-10-22 20:44:17 +0530

    using browser router instead of router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import history from "./history.js";
 import App from "./App.js";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
