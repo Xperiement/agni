2d984c020b65515efe655534fd3e1578bdc89574
Sourav Gain <souravgain605@gmail.com>
2021-10-22 14:27:23 +0530

    updated app & index structure

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import Downloads from "./pages/Downloads";
 import Home from "./pages/Home";
 import { Switch, Route } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useScrollDirection } from "react-use-scroll-direction";
+import Installation from "./pages/Installation";
+import axios from "axios";
+import NotFound from "./components/Downloads/NotFound";
+import MobileNav from "./components/MobileNav";
 
 function App() {
+  const { scrollDirection } = useScrollDirection();
+  const [hide, setHide] = useState(false);
+  const [showDevices, setShowDevices] = useState(false);
   const [dark, setDark] = useState(false);
+  const [state, setState] = useState(() => ({
+    started: 1970,
+    ends: 2070,
+    improvements: [],
+    credits: [],
+    support_groups: [],
+    donate: [],
+  }));
 
   useEffect(() => {
     const local_dark = localStorage.getItem("dark");
@@ -17,6 +33,24 @@ function App() {
       document.documentElement.setAttribute("data-theme", "dark");
     }
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/Xperiement/agni_data/main/base.json"
+      )
+      .then((res) => {
+        if (res.status === 200) setState(res.data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (scrollDirection === "DOWN") {
+      setHide(true);
+    } else if (scrollDirection === "UP") {
+      setHide(false);
+    }
+  }, [scrollDirection]);
 
   const switchTheme = () => {
     if (dark) {
@@ -34,12 +68,30 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/">
-          <Home dark={dark} dark_toggle={switchTheme} />
+          <Home dark={dark} dark_toggle={switchTheme} state={state} />
+        </Route>
+        <Route exact path="/installation">
+          <Installation dark={dark} dark_toggle={switchTheme} state={state} />
         </Route>
         <Route exact path="/downloads/:name?/:type?">
-          <Downloads />
+          <Downloads
+            dark={dark}
+            dark_toggle={switchTheme}
+            showDevices={showDevices}
+            setShowDevices={setShowDevices}
+          />
+        </Route>
+        <Route exact path="/404">
+          <NotFound />
         </Route>
       </Switch>
+      <MobileNav
+        dark={dark}
+        dark_toggle={switchTheme}
+        hide={hide}
+        setShowDevices={setShowDevices}
+        showDevices={showDevices}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import history from "./history.js";
 import App from "./App.js";
 import "remixicon/fonts/remixicon.css";
 import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router children={<App />} />
+    <Router history={history}>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
