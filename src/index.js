c9a3d4994b5c56e7803f6e8aadaee9d418779a32
Sourav Gain <souravgain605@gmail.com>
2021-10-22 17:44:19 +0530

    added some pwa configuration

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import history from "./history.js";
 import App from "./App.js";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import "remixicon/fonts/remixicon.css";
 import "./styles/index.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router history={history}>
+    <Router basename="/agni" history={history}>
       <App />
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
