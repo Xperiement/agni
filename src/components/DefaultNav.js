eeb18c13d9ef1c903bab6ca6952e14769c5e8ec7
Sourav Gain <souravgain605@gmail.com>
2021-10-22 14:26:21 +0530

    modified default nav

--- src/components/DefaultNav.js
+++ src/components/DefaultNav.js
@@ -1,17 +1,40 @@
 import React from "react";
 import "./defaultnav.scss";
 import Logo from "../assets/agni.png";
-import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import ModeButton from "./ModeButton";
 
 export default function DefaultNav({ dark, dark_toggle }) {
   return (
     <nav className="default">
-      <img className="navHome" src={Logo} alt="" />
-      <Link to="/downloads">Downloads</Link>
-      <div className="navButton">Installation</div>
-      <div className="navButton">Support</div>
-      <div className="navButton">About</div>
+      <HashLink smooth to="/#home">
+        <img className="navHome" src={Logo} alt="" />
+      </HashLink>
+
+      {/* <img className="navHome" src={Logo} alt="" /> */}
+
+      <HashLink smooth to="/downloads">
+        Downloads
+      </HashLink>
+      <HashLink smooth to="/installation#iTop">
+        Installation
+      </HashLink>
+      <HashLink smooth to="/#footer" className="support">
+        Support
+      </HashLink>
+      <HashLink
+        smooth
+        to={{ pathname: "/", hash: "#about" }}
+        // onSuspend={() => {
+        //   const x = document.createElement("a");
+        //   x.href = "#about";
+        //   x.click();
+        //   console.log("Fuck YOu");
+        // }}
+        className="about"
+      >
+        About
+      </HashLink>
 
       <ModeButton enabled={dark} toggle={dark_toggle} />
     </nav>
